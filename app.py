da27cc2f2e8f1fb33977327de6f492a85d4d839e
vinayak0000-code <73603869+vinayak0000-code@users.noreply.github.com>
2022-07-30 10:39:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(_name_)
+model = pickle.load(open('linearregression.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
+
+
+if _name_ == "_main_":
+    app.run(debug=True)
