17d0e3586ec26d6ac0aa9d0e5adef1816503d8a4
vinayak0000-code <73603869+vinayak0000-code@users.noreply.github.com>
2022-07-30 11:00:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model = pickle.load(open('linearregression.pkl','rb'))
 @app.route('/')
 def home():
   
-    return render_template("index.html")
+    return render_template("index2.html")
   
 @app.route('/predict',methods=['GET'])
 def predict():
